e70ed5bac0ecbe8627d1ba07ce640467144143bb
Anderson Menezes <anderson.stifip@gmail.com>
2024-02-24 04:04:21 -0300

    feat: create module app-material - creation module shared for imports components Angular Material

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,20 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 
 
+import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products/products.component';
 
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
   ],
   imports: [
     CommonModule,
     ProductsRoutingModule,
-    MatTableModule
+    AppMaterialModule
   ]
 })
 export class ProductsModule { }
